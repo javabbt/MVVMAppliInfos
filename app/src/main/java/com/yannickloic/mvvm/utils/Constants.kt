8f6011bdfd7e8c96f60931f41f623b6bd00907f9
Yannick Loic <ngapmennyamiyannickloic@gmail.com>
2021-11-04 07:29:45 -0700

    Update Constants.kt

--- app/src/main/java/com/yannickloic/mvvm/utils/Constants.kt
+++ app/src/main/java/com/yannickloic/mvvm/utils/Constants.kt
@@ -4,6 +4,8 @@ class Constants {
     companion object{
         /**
         **cle d'api
+        **cette cle est postee sur github juste pour les besoins du projet
+        **en temps normal cette classe ne devrait pas etre visible 
          **/
         const val API_KEY= "c03689e038ba46c2b1e43b3304b22800"
         const val BASE_URL= "https://newsapi.org"
